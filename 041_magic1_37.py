5e56b6a822a6fa9729187204a8d846ea7b581daf
Glimmer226 <41518037@qq.com>
2019-04-02 23:32:58 +0800

    Update the magic function of Class
    update for class 41
    update for class 45
    add new code for class 46

--- 041_magic1_37.py
+++ 041_magic1_37.py
@@ -22,9 +22,114 @@ class Nint(int):
         return int.__new__(cls, arg)
 
 
+class Nstr(str):
+    def __sub__(self, other):
+        return self.replace(other, ' ')
+
+
+class Bstr(str):
+    def __lshift__(self, other):
+        return self[other:] + self[:other]
+
+    def __rshift__(self, other):
+        return self[:-other] + self[-other:]
+
+
+class Cstr:
+    def __init__(self, arg=''):
+        if isinstance(arg, str):
+            self.total = 0
+            for each in arg:
+                self.total += ord(each)
+        else:
+            print("Wrong parameters")
+
+    def __add__(self, other):
+        return self.total + other.total
+
+    def __sub__(self, other):
+        return  self.total - other.total
+
+    def __mul__(self, other):
+        return self.total * other.total
+
+    def __truediv__(self, other):
+        return self.total / other.total
+
+    def __floordiv__(self, other):
+        return self.total // other.total
+
+
+class C:
+    @staticmethod
+    def static(arg1, arg2, arg3):
+        print(arg1, arg2, arg3)
+
+    def nostatic(self):
+        print("I'm the normal method!")
+
+
+class D:
+    def __init__(self, *args):
+        if not args:
+            print("No args input")
+        else:
+            print("%d args have been inputted, each is:" % len(args))
+            for each in args:
+                print(each, end=' ')
+
+
+class Word(str):
+    def __new__(cls, word):
+        if ' ' in word:
+            print("Value contains spaces. Truncating to first space.")
+            word = word[:word.index(" ")]
+        return str.__new__(cls, word)
+
+    def __lt__(self, other):
+        return len(self) < len(other)
+
+    def __gt__(self, other):
+        return len(self) > len(other)
+
+    def __ge__(self, other):
+        return len(self) >= len(other)
+
+    def __le__(self, other):
+        return len(self) <= len(other)
+
+
 if __name__ == "__main__":
     c = C2F(32)
     print(c)
     print(Nint(123))
     print(Nint(1.5))
     print(Nint("abc"))
+    a = Nstr("I love FishC.com! iiiiiii")
+    b = Nstr("i")
+    print(a - b)
+    c = Bstr('I love FishC.com!')
+    print(c << 3)
+    print(c >> 3)
+    d = Cstr('FishC')
+    e = Cstr("love")
+    print(d + e)
+    print(d - e)
+    print(d * e)
+    print(d / e)
+    print(d // e)
+    print("*" * 50)
+    c1 = C()
+    c2 = C()
+    print(c1.static is C.static)
+    print(c1.nostatic is C.nostatic)
+    print(c1.static)
+    print(c2.static)
+    print(C.static)
+    print(c1.nostatic)
+    print(c2.nostatic)
+    print(C.nostatic)
+    print(c1.static(1, 2, 3))
+    print(C.static(1, 2, 3))
+    print("*" * 50)
+    d = D(12,3,4,5,6,7,8,98,90,0)
